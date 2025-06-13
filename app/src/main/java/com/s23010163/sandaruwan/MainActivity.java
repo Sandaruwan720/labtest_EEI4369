a9c47f14181ce9c952b14a01abcb4a35bb34ae08
Tharuka Sandaruwan <154860798+Sandaruwan720@users.noreply.github.com>
2025-06-13 19:46:30 +0530

    create login page

--- app/src/main/java/com/s23010163/sandaruwan/MainActivity.java
+++ app/src/main/java/com/s23010163/sandaruwan/MainActivity.java
@@ -1,6 +1,9 @@
 package com.s23010163.sandaruwan;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,15 +13,28 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button btnGetStart;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        btnGetStart = findViewById(R.id.button);
+        btnGetStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Navigate to LoginActivity when button clicked
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
         });
     }
 }
